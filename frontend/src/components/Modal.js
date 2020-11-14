430ee890e3e3ad53e7c98d97f1706f606423ec5a
Edson Ayllon <eayllon1@gmail.com>
2020-11-14 18:15:57 -0500

    add slider

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,7 +1,8 @@
 import { useContext, useState, useEffect } from 'react';
 import { 
     Button,
-    InputNumber
+    InputNumber,
+    Slider
 } from 'antd' 
 import { GlobalState } from 'globalState';
 
@@ -26,8 +27,9 @@ export const Modal = () => {
 
     const setMaxBalance = () => setInvestment(balanceDecimals());;
 
-    const calcPercentInvestment = (percent) => {
-        if (percent > 1) percent = 1;
+    const calcInvestmentFromPercent = (percent) => {
+        if (percent > 100) percent = 100;
+        setInvestment( balanceDecimals() * percent / 100 );
     }
 
     const getBalance = () => {
@@ -41,34 +43,47 @@ export const Modal = () => {
         }
     }
 
+    const marks = {
+        0: '0%',
+        25: '25%',
+        50: '50%',
+        75: '75%',
+        100: '100%'
+    };
+
     useEffect(getBalance, [])
     return (
         <div className="modal-container">
             <div className="modal-content flex row">
-                <div className="flex column flex-one padd16">
+                <div className="flex column flex-one padd-32">
                     <h2>Invest in {poolName}</h2>
                     <hr/>
                     <h3 className="caps">Summary</h3>
                     <hr/>
                 </div>
-                <div className="flex column lighten flex-two padd16 space-between">
+                <div className="flex column lighten flex-two padd-32 space-between">
                     <div className="flex column">
-                        <div className="row space-between">
-                            <a onClick={setMaxBalance}>balance: {balanceDecimals()}</a>
+                        <div className="row flex space-between mb-16">
+                            <span className="caps">Invest with</span>
+                            <a onClick={setMaxBalance}>Balance: {balanceDecimals().toFixed(3)}</a>
                         </div>
-                        <div className = "row">
+                        <div className = "flex row">
+                            <div className = "input-symbol">
+                                $ sUSD
+                            </div>
                             <InputNumber 
-                                className="width-100" min={0} defaultValue={3} 
+                                className="width-100 symbol-padding text-align-right" min={0} defaultValue={3} 
                                 onChange={onInput} size="large" color="blue-1" 
                                 value={investment}/>
                         </div>
+                        <Slider marks={marks} defaultValue={0} onChange={value => calcInvestmentFromPercent(value)} value ={100 * investment / balanceDecimals()}/>
                         
                     </div>
                     <div className="row width-100 flex-end flex" >
                         <Button  className="flex-one" onClick={handleCloseModal} size="large">
                             Cancel
                         </Button>
-                        <Button className="mgl-16 flex-one" onClick={handleCloseModal} size="large" type="primary">
+                        <Button className="ml-16 flex-one" onClick={handleCloseModal} size="large" type="primary">
                             Confirm
                         </Button>
                     </div>
